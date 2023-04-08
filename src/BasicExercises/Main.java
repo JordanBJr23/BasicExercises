3b5bb7db4b7e432b031d8ad40ecf3b7c992ace16
JordanBjr23 <jordanmarz12@gmail.com>
2023-04-08 16:51:17 -0600

    se agrega un cambio

--- src/BasicExercises/Main.java
+++ src/BasicExercises/Main.java
@@ -7,7 +7,7 @@ public class Main {
     
     public static void main(String[] args) {
      BasicExercises call = new BasicExercises();
-     
+        System.out.println("new message");
         call.vowels();
     }
     
