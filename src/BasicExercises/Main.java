c1b842efb8bd13f77c1eb14068cd474a5844faa0
JordanBjr23 <jordanmarz12@gmail.com>
2023-04-08 16:54:09 -0600

    s

--- src/BasicExercises/Main.java
+++ src/BasicExercises/Main.java
@@ -7,7 +7,7 @@ public class Main {
     
     public static void main(String[] args) {
      BasicExercises call = new BasicExercises();
-        System.out.println("new message");
+        System.out.println("new messag");
         call.vowels();
     }
     
